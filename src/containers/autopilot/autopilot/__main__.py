c360e612876af020e848191f6a776202319296e3
lh.laq <lh.laquay@gmail.com>
2025-04-10 00:22:30 +0200

    listen nmea first

--- src/containers/autopilot/autopilot/__main__.py
+++ src/containers/autopilot/autopilot/__main__.py
@@ -7,11 +7,11 @@ from nats_integration import nats_integration
 from ship import listen_nmea
 
 async def main():
+  listen_nmea_task = asyncio.create_task(listen_nmea())
   ap = Autopilot()
   ap_task = asyncio.create_task(ap.run_loop())
   modbus_task = asyncio.create_task(modbus_integration())
   nats_task = asyncio.create_task(nats_integration())
-  listen_nmea_task = asyncio.create_task(listen_nmea())
   await asyncio.gather(
     ap_task,
     modbus_task,
